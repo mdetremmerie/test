6d703e0e62fe6852c5ca48f6464bf0a527cbb2b9
mdetremmerie <mathieu.detremmerie@student.uclouvain.be>
2017-04-15 19:53:50 +0200

    premier commit

---
+++ hello.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#define SIZE 4
+int main() 
+{
+printf("Hello World\n");
+int reste;
+reste=10%10;
+printf("%d\n",reste);
+int size=4;
+int tab[SIZE]={1,0,0,0};
+printf("%d",tab[0]);
+}
